983f0551abd7c79113227d266dc458829643465b
john_wisneski <john.j.wisneski@gmail.com>
2020-12-21 19:23:47 -0500

    commit tree b02bba2967e29f973ce182fdcca5b3cac3b04cbf

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
+# hi_scores = [20203, 1023, 30111, 3204, 32023]
+# my_select(tas) {|score| score > 20000 }
+
 def my_select(collection)
- # your code here!
+    i = 0
+    new_block = []
+    while i < collection.length
+        if yield(collection[i]) == true
+            new_block << collection[i]
+        end
+        i += 1
+    end
+    new_block
 end
